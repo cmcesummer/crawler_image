6d26e9a13c5e901b917f06a8187b9d1e54e0c0c3
zhanghao05 <zhanghao05@mail.jj.cn>
2017-12-21 16:27:50 +0800

    爬取具体图片，可能还有问题 大概是图pain下载不完整

--- index.js
+++ index.js
@@ -49,45 +49,60 @@ const get_item_url_arr = (page_arr) => {
     })
 }
 
-// $http.get('https://tieba.baidu.com/p/comment?tid=5171602458&pid=108295392615&pn=1&t=1513685817724&red_tag=1472410715')
-//     .set('cookie', cookie)
-//     .set('Host', 'tieba.baidu.com')
-//     .set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36')
-//     .then(res => {
-//         if(res.status != 200) throw res.status
-//         let a_arr = [];
- 
-//         const $ = cheerio.load(res.text);
-        
-//         total_page = Number($('.pager_theme_2').children().last().attr('href').substring(1))
-
-//         const page_url_arr = get_page_url(total_page);
-
-//         get_item_url_arr(page_url_arr);
-
-//     }).catch(err => {
-//         console.log(err)
-//     })
-
-
-$http.get('https://tieba.baidu.com/p/5430286064')
+$http.get('https://tieba.baidu.com/p/comment?tid=5171602458&pid=108295392615&pn=1&t=1513685817724&red_tag=1472410715')
+    .set('cookie', cookie)
     .set('Host', 'tieba.baidu.com')
-    .set('User-Agent', 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Mobile Safari/537.36')
+    .set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36')
     .then(res => {
+        if(res.status != 200) throw res.status
+        let a_arr = [];
+ 
         const $ = cheerio.load(res.text);
-        let img_arr = [];
-        $('.j_media_thumb_holder.img_placeholder').each(function() {
-            img_arr.push($(this).attr('data-url'));
-        })
-        console.log($('title').text())
-        // fs.writeFile(path.join(__dirname, './res_file/BDE_Image.json'), JSON.stringify(img_arr), 'utf-8', err => {
-        //     if (err) throw err;
-        //     console.log('write baidu over')
-        // })
+        
+        total_page = Number($('.pager_theme_2').children().last().attr('href').substring(1))
+
+        const page_url_arr = get_page_url(total_page);
+
+        get_item_url_arr(page_url_arr);
+
+    }).catch(err => {
+        console.log(err)
     })
 
 
+// request('http://imgsrc.baidu.com/forum/pic/item/60789b2ad40735fa61d9010195510fb30f24083a.jpg').pipe(fs.createWriteStream(path.join(__dirname, '/naa.jpg')))
 
+// var img_arr = [
+//     "http://imgsrc.baidu.com/forum/pic/item/81f0fa02738da9779d8d31b4bb51f8198718e375.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/3d637f8ca9773912168432fef3198618377ae275.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/b8eda5763912b31b16ce7ab68d18367adbb4e175.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/62173513b31bb0515f8604b73d7adab44bede075.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/f272bf1ab051f8192e87b4d5d1b44aed2f73e775.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/787bbc50f81986189fe5581b41ed2e738ad4e675.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/7b31f4188618367a702bc84225738bd4b21ce575.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/33798a19367adab4e172acdc80d4b31c8601e475.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/4d783a7bdab44aed82ec097bb81c8701a08bfb75.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/fd1ad6b54aed2e73264b31b38c01a18b86d6fa75.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/11d446ec2e738bd41d8305aeaa8b87d6267ff975.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/818d22728bd4b31c289e23248cd6277f9f2ff875.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/e51387d5b31c8701111405792c7f9e2f0608ff75.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/40b4bf1d8701a18b3649a5d0952f07082938fe75.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/787c8b00a18b87d695e01c800c0828381e30fd75.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/4c61ad8a87d6277f2db085a723381f30e824fc75.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/6aeb8bd7277f9e2fb397aa971430e924b999f375.jpg",
+//         "http://imgsrc.baidu.com/forum/pic/item/2c719cee76c6a7efe7a41fb9f6faaf51f2de6614.jpg"
+// ];
+// async.mapLimit(img_arr, 3, (url, callback) => {
+//     const img_type = url.substring(url.lastIndexOf('.'));
+//     const name = img_arr.indexOf(url);
+//     console.log(url)
+
+//     request(url).pipe(fs.createWriteStream(path.join(__dirname, './res_file/img' , name + img_type)))
+//     callback(null)
+    
+// }, (err, res) => {
+//     console.log('over')
+// })
 
 
 
--- page.js
+++ page.js
@@ -15,29 +15,27 @@ console.log(num)
 fs.readFile(path.join(__dirname, './res_file/item_url_arr.json'), 'utf-8', (err, data) => {
     if(err) throw err;
     const item_arr = JSON.parse(data)[(parseInt(num) - 1)];
-    console.log(item_arr);
-    async.mapLimit(item_arr, 5, (url, callback) => {
+    // console.log(item_arr);
+    async.mapLimit(item_arr, 1, (url, callback) => {
 
-        console.log('req:' + url )
+        // console.log('req:' + url )
         
         $http.get(url)
         .set('Host', 'tieba.baidu.com')
         .set('User-Agent', 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Mobile Safari/537.36')
         .then(res => {
-            console.log('res:' + url) 
+            // console.log('res:' + url) 
 
             const $ = cheerio.load(res.text);
             let img_arr = [];
             const title = $('title').text();
-            const dir_name = path.join(__dirname, './res_file/', title);
+            const dir_name = path.join(__dirname, '../res_file/', title);
 
             $('.j_media_thumb_holder.img_placeholder').each(function() {
                 let little_url = $(this).attr('data-url');
                 const url = decodeURIComponent(little_url.substring(little_url.indexOf('src=http') + 4));
                 img_arr.push(url);
             })
-
-            callback(null, {dir_name, img_arr})
 
             if (!fs.existsSync(dir_name)) {
                 fs.mkdirSync(dir_name);
@@ -50,18 +48,26 @@ fs.readFile(path.join(__dirname, './res_file/item_url_arr.json'), 'utf-8', (err,
 
             fs.writeFile(path.join(dir_name, '/information.json'), JSON.stringify(information), 'utf-8', err => {
                 if (err) throw err;
-                console.log('write '+ dir_name +' over')
+                // console.log('write '+ dir_name +' over')
+                callback(null, {dir_name, img_arr})
             })
 
-            // async.mapLimit(img_arr, 3, (url, callback) => {
-            //     request
+            async.mapLimit(img_arr, 3, (url, callback) => {
+                const img_type = url.substring(url.lastIndexOf('.'));
+                const name = img_arr.indexOf(url);
+                request({
+                    url: url
+                })
+                .on('error', err => console.log(err))
+                .pipe(fs.createWriteStream(path.join(dir_name, name + img_type)))
                 
-            // }, (err, res) => {
+                callback(null);
+            }, (err, res) => {
+                console.log('img '+ title +'over')
+            })
 
-            // })
-            
-
-
+        }).catch(err => {
+            console.log('$http err : ' + err)
         })        
 
     }, (err, res) => {
